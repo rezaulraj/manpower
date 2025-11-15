62479a19f0e69bc61b9c660e5ad5a90cbd0ed415
rezaulraj <rezaul.coderpro@gmail.com>
2025-11-15 22:35:19 +0600

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 👈 this tells Next.js to create /out with HTML files
-  images: {
-    unoptimized: true, // Required for next/image when using `next export`
-  },
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
