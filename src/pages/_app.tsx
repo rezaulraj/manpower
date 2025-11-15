407551e4e30ea363dd4e5778684bff860a0f8a32
rezaulraj <rezaul.coderpro@gmail.com>
2025-11-15 22:09:13 +0600

    update app commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,15 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 
-const inter = Montserrat({
+const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "700", "900"],
-  variable: "--font-inter", // sets the font in a CSS variable
+  variable: "--font-inter",
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={` ${inter.className} antialiased`}>
+    <main className={` ${montserrat.className} antialiased`}>
       <Component {...pageProps} />
     </main>
   );
